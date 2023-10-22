b9447f24b9d1acc81a0d0afaf586e08f0a88991d
denis <G00403126@atu.ie>
2023-10-22 22:58:51 +0100

    UserDetails

---
+++ src/main/java/ie/atu/hello/UserDetails.java
@@ -0,0 +1,13 @@
+package ie.atu.hello;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDetails {
+    private String name;
+    private String email;
+}
